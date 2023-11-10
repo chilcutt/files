40cc3451e675eef0035d5abc294a273870995f37
Kyle Chilcutt <kyle@chilcutt.com>
2023-11-10 08:27:42 -0800

    Fix warning when starting neovim
    Replace update_capabilities with default_capabilities.

--- configs/system/.config/nvim/init.lua
+++ configs/system/.config/nvim/init.lua
@@ -220,7 +220,7 @@ end
 
 --------------------------------------------------------------------------------
 -- nvim-lspconfig
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Set up typescript lsp
 require("lspconfig")["tsserver"].setup({
